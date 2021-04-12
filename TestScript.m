ed86bdd2973e8ea3e6882ac4a0cb8e53f9fe3fe2
NatalijaDjuric <82355761+NatalijaDjuric@users.noreply.github.com>
2021-04-12 06:44:10 +0200

    Test script update
    Added comparation of all elements from original array with corresponding elements from decompressed array

--- TestScript.m
+++ TestScript.m
@@ -2,11 +2,16 @@ clear all
 clc
 
 disp('creating array of 64-bit timestamps')
-time = datetime('2016-07-29 10:05:24') + calmonths(1:3);
+% by changing Ranges or datetime we can have different timestamp arrays
+timeRangeHours = 5;
+timeRangeMonths = 3;
+time = datetime('2021-01-01 10:30:15') + hours(1:timeRangeHours); %calmonths(1:timeRangeMonths) can also be used
 timestampsArray = posixtime(time);
 timestampsArray = uint64(timestampsArray);
 disp('original array:')
 timestampsArray
+numberOfTimestamps = length(timestampsArray);
+
 
 disp('creating array of bytes')
 %vector<uint8_t> compress(vector<uint64_t> original);
@@ -20,3 +25,16 @@ disp('getting the original array from array of bytes')
 decompressedArray = Decompress(compressedArray);
 disp('array after decompression')
 decompressedArray
+
+%% comparing original array and array we got after compression and decompression
+failFlag = false;
+for i=1:1:numberOfTimestamps
+    if (timestampsArray(i) ~= decompressedArray(i))
+       disp('decompression failed! Array is not the same like the original')
+       failFlag = true;
+       break;
+    end
+end
+if (failFlag == false)
+   disp('job done! Decompressed array matches original!') 
+end
